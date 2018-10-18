f8919d33aed6d75eec9c9d8c043fc6d9cdae9d79
HASSANI Youssef <hassanyo@hostname.iut2.upmf-grenoble.fr>
2018-10-18 10:51:52 +0200

    sds

---
+++ view/main.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Gzer</title>
+</head>
+
+<body>
+  <nav class="site-header sticky-top py-1">
+    <div class="container d-flex flex-column flex-md-row justify-content-between">
+      <h1>Gzer</h1>
+      <a class="py-2 d-none d-md-inline-block" href="#">Boutique</a>
+      <a class="py-2 d-none d-md-inline-block" href="#">Annonces</a>
+      <a class="py-2 d-none d-md-inline-block" href="#">Se connecter</a>
+    </div>
+  </nav>
+</body>
+
+</html>
