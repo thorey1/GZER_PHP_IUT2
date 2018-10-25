e7ba8bb344902e4ff5346c35dde01300db07f0c8
TOSTON Remi <tostonr@hostname.iut2.upmf-grenoble.fr>
2018-10-25 13:37:30 +0200

    25/10/18 deposer annonce ctrl et create sql

--- controler/deposer_annonce.ctrl.php
+++ controler/deposer_annonce.ctrl.php
@@ -6,27 +6,52 @@ require_once('../model/GzerDAO.class.php');
 // PARTIE USAGE DU MODELE
 //////////////////////////////////////////////////////////////////////////////
 
-
+  date_default_timezone_set('UTC');
 
   $config = parse_ini_file('../config/config.ini');
   $dao = new GzerDAO($config['database_path']);
+
   $categorie = $_GET['categorie'];
-  $style = $_GET['style'];
   $titre = $_GET['titre'];
   $prix = $_GET['prix'];
   $quartier = $_GET['quartier'];
   $numA= count(getAnnoncesAll())+1;
+  $dateA= date("j, n, Y");
+  $auteurA= getAuteurA();
   if (isset($categorie)== 'prestation'){
-    $insert = "INSERT INTO annonce (numA, categorieA, titreA, prixA, localisationA, auteurA, dateA, , styleA)
-    VALUES (:numA, :categorieA, :titreA, :prixA, :localisationA, :auteurA, :dateA, )";
+    $style= $_GET['style'];
+
+    $insert = "INSERT INTO annonce (numA, titreA, prixA, localisationA, auteurA, dateA, categorieA, styleA) VALUES (:numA, :titreA, :prixA, :localisationA, :auteurA, :dateA, :categoieA, :styleA )";
     $statement = $db->prepare($insert);
 
-  }
-  $insert = "INSERT INTO annonce (numA, categorieA, titreA, prixA, localisationA, auteurA, dateA, categorieA, styleA)
-  VALUES (:numA,:)";
-  $statement = $db->prepare($insert);
+    // Bind parameters to statement variables
+      $stmt->bindValue(':numA', $numA);
+      $stmt->bindValue(':titreA', $titre);
+      $stmt->bindValue(':prixA', $prix);
+      $stmt->bindValue(':localisationA', $quartier);
+      $stmt->bindValue(':auteurA', $auteurA);
+      $stmt->bindValue(':dateA', $dateA);
+      $stmt->bindValue(':categorieA', $categorie);
+      $stmt->bindValue(':styleA', $style);
+    }
+  else {
+    $style= '';
 
-''
+    $insert = "INSERT INTO annonce (numA, titreA, prixA, localisationA, auteurA, dateA, categorieA, styleA) VALUES (:numA, :titreA, :prixA, :localisationA, :auteurA, :dateA, :categoieA, :styleA )";
+    $statement = $db->prepare($insert);
+
+    // Bind parameters to statement variables
+      $stmt->bindValue(':numA', $numA);
+      $stmt->bindValue(':titreA', $titre);
+      $stmt->bindValue(':prixA', $prix);
+      $stmt->bindValue(':localisationA', $quartier);
+      $stmt->bindValue(':auteurA', $auteurA);
+      $stmt->bindValue(':dateA', $dateA);
+      $stmt->bindValue(':categorieA', $categorie);
+      $stmt->bindValue(':styleA', $style);
+    }
+
+    $stmt->execute();
 //////////////////////////////////////////////////////////////////////////////
 // PARTIE USAGE DE LA VUE
 //////////////////////////////////////////////////////////////////////////////
--- model/data/create.sql
+++ model/data/create.sql
@@ -1,21 +1,12 @@
-CREATE TABLE annoncesMat (
-  numAM INTEGER,
-  categorieAM STRING,
-  titreAM STRING,
-  prixAM INTEGER,
-  localisationAM STRING,
-  auteurAM STRING,
-  dateAM date
-);
-
-CREATE TABLE annoncesPrest (
-  numAP INTEGER,
-  titreAP STRING,
-  styleAP STRING,
-  prixAP INTEGER,
-  localisationAP STRING,
-  auteurAP STRING,
-  dateAP date
+CREATE TABLE annonces (
+  numA INTEGER,
+  titreA STRING,
+  prixA INTEGER,
+  localisationA STRING,
+  auteurA STRING,
+  dateA date,
+  categorieA STRING,
+  styleA STRING
 );
 
 CREATE TABLE membres (
