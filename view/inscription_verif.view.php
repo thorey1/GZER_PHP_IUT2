0ef7c6e896534d86164376c484b91fb1a6e92b2b
THORE Yann <thorey@hostname.iut2.upmf-grenoble.fr>
2018-11-08 17:42:32 +0100

    inscription5

---
+++ view/inscription_verif.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Gzer inscription</title>
+  <link rel="stylesheet" href="../view/css/deposer_annonce.view.styles.css" />
+</head>
+<body>
+
+  <div class="topnav">
+    <a href="../controler/main.ctrl.php">GZER</a>
+    <a href="../controler/deposer_annonce.ctrl.php">Déposer une annoce</a>
+    <a href="../controler/annonces.ctrl.php">Annonces</a>
+    <a class="active" href="../controler/seconnecter.ctrl.php">Se connecter</a>
+  </div>
+
+  <div class="navbar">
+    <a href="../view/infos.view.php">Informations légales</a>
+    <a href="../view/a_propos.view.php">A propos</a>
+  </div>
+
+  <div id=container style="padding-left:16px">
+    <p>Vous êtes bien inscrit</p>
+
+  </div>
+
+</body>
+</html>
